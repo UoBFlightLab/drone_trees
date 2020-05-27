175ff76f3575dd861010fe40763233b926b3a3c0
Arthur <arthur.richards@bristol.ac.uk>
2020-05-27 15:00:20 +0100

    New round of Linux testing.  TODO provide Linux alternative for the hardwired windows exec in text_bridge.  Also apparent bug in test_square.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='drone_trees',
+      version='1.0.1',
+      description='Behaviour trees for drone control',
+      url='https://github.com/UoBFlightLab/drone_trees',
+      author='UoBFlightLab',
+      author_email='flightlab@example.com',
+      license='MIT',
+      packages=['drone_trees'],
+      zip_safe=False)
